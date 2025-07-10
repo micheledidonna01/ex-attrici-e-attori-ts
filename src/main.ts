67d0945029df68461dd0d6d87bf7b62900e60b97
micheledidonna01 <micheledidonna337@gmail.com>
2025-07-10 15:47:47 +0200

    feat: milestone 3

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 
 
 
-type Persona = {
+type Person = {
   readonly id: number,
   readonly name: string,
   birth_year: number,
@@ -11,9 +11,64 @@ type Persona = {
   image: string
 }
 
-type Actress = Persona &
+type Actress = Person &
 {
   most_famous_movies: [string, string, string],
   awards: string,
-  nationality: "American" | "British"| "Australian"| "Israeli - American"| "South African" | "French" | "Indian" | "Israeli" | "Spanish" | "South Korean" | "Chinese"
+  nationality: "American" | "British" | "Australian" | "Israeli - American" | "South African" | "French" | "Indian" | "Israeli" | "Spanish" | "South Korean" | "Chinese"
 }
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+    const result : unknown = await response.json();
+
+    if (!response.ok) {
+      throw new Error("Problemi con il server");
+    }
+
+    if (!isActress(result)) {
+      throw new Error("errore");
+    }
+    console.log(result);
+    return result
+
+
+  } catch (errore) {
+    console.error("errore con il server")
+    return null;
+  }
+}
+
+
+function isActress(result: unknown): result is Actress {
+  if (
+      result &&
+      typeof result === "object" &&
+      "id" in result &&
+      typeof result.id === "number" &&
+      "name" in result &&
+      typeof result.name === "string" &&
+      "birth_year" in result &&
+      typeof result.birth_year === "number" &&
+      (!("death_year" in result) || typeof (result as any).death_year === "number") &&
+      "biography" in result &&
+      typeof result.biography === "string" &&
+      "image" in result &&
+      typeof result.image === "string" &&
+      "most_famous_movies" in result &&
+      Array.isArray(result.most_famous_movies) &&
+      "awards" in result &&
+      typeof result.awards === "string" &&
+      "nationality" in result &&
+      typeof result.nationality === "string"
+    ) 
+    {
+    return true;
+    }
+
+  return false;
+}
+
+getActress(1);
+
