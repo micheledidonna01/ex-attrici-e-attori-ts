eadbbf14008e4f9dfc2f926cbee769507e143b46
micheledidonna01 <micheledidonna337@gmail.com>
2025-07-11 13:02:31 +0200

    feat: bonus 2

--- src/main.ts
+++ src/main.ts
@@ -154,7 +154,6 @@ getAllActresses();
 
 // getActresses([1,2,3,4])
 
-createActress
 
 
 function generaID(): number {
@@ -180,9 +179,169 @@ function createActress(dati: Omit<Actress, "id">): Actress {
 // }
 
 //SOLUZIONE ALTERNATIVA
-function updateActress(actress: Actress, updates: Partial<Omit<Actress, "id" | "name">>): Actress{
-  return{
+function updateActress(actress: Actress, updates: Partial<Omit<Actress, "id" | "name">>): Actress {
+  return {
     ...actress,
     ...updates,
   }
 }
+
+type Actor = Person &
+{
+  known_for: [string, string, string]
+  awards: [string, string?] | [string]
+  nationality:
+  | "American"
+  | "British"
+  | "Australian"
+  | "Israeli - American"
+  | "South African"
+  | "French"
+  | "Indian"
+  | "Israeli"
+  | "Spanish"
+  | "South Korean"
+  | "Chinese"
+  | "Scottish"
+  | "New Zealand"
+  | "Hong Kong"
+  | "German"
+  | "Canadian"
+  | "Irish"
+
+}
+
+
+function isActor(dati: unknown): dati is Actor {
+  if (
+    typeof dati === "object" &&
+    dati !== null &&
+    "id" in dati &&
+    typeof dati.id === "number" &&
+    "name" in dati &&
+    typeof dati.name === "string" &&
+    "birth_year" in dati &&
+    typeof dati.birth_year === "number" &&
+    (!("death_year" in dati) || typeof (dati as any).death_year === "number") &&
+    "biography" in dati &&
+    typeof dati.biography === "string" &&
+    "image" in dati &&
+    typeof dati.image === "string" &&
+    "known_for" in dati &&
+    Array.isArray(dati.known_for) &&
+    dati.known_for.length === 3 &&
+    dati.known_for.every((m: unknown) => typeof m === "string") &&
+    "awards" in dati &&
+    Array.isArray(dati.awards) && dati.awards.length > 0 && dati.awards.length <= 2 &&
+    dati.awards.every((a: unknown) => typeof a === "string") &&
+    "nationality" in dati &&
+    typeof dati.nationality === "string"
+  ) {
+    return true;
+  }
+  return false;
+}
+
+async function getActor(id: number): Promise<Actor | null> {
+
+  try {
+    const promise = await fetch(`http://localhost:3333/actors/${id}`);
+
+    if (!promise.ok) {
+      throw new Error(promise.status + promise.statusText);
+    }
+
+    const obj:unknown = await promise.json();
+
+    if (!isActor(obj)) {
+      throw new Error('Errore dal controllo dell\'oggetto');
+    }
+    console.log("Actor", obj)
+    return obj;
+
+  } catch (errore) {
+    if (errore instanceof Error) {
+      console.error(errore.message);
+    } else {
+      console.error('Erorre sconosciuto', errore);
+    }
+    return null;
+  }
+}
+
+getActor(2);
+
+
+async function getActorAll(): Promise<Actor[]> {
+
+  try {
+    const promise = await fetch(`http://localhost:3333/actors`);
+
+    if (!promise.ok) {
+      throw new Error(promise.status + promise.statusText);
+    }
+
+    const arrObj: unknown = await promise.json();
+    console.log(arrObj)
+
+    if (!(arrObj instanceof Array)) {
+      throw new Error('Formato dati non valido');
+    }
+
+
+    const arrObjValid: Actor[] = arrObj.filter(isActor);
+    console.log(isActor(arrObj[0]));
+    console.log("Attori: ", arrObjValid);
+    return arrObjValid;
+
+  } catch (errore) {
+    if (errore instanceof Error) {
+      console.error(errore.message);
+    } else {
+      console.error('Erorre sconosciuto', errore);
+    }
+    return [];
+  }
+}
+
+getActorAll();
+
+
+async function getActors(ids: number[]): Promise<(Actor | null)[]>{
+
+  try{
+    const promise = ids.map(id => getActor(id));
+    const array = await Promise.all(promise);
+
+    console.log(array);
+    return array;
+
+  }catch(errore){
+    if (errore instanceof Error) {
+      console.error(errore.message);
+    } else {
+      console.error('Erorre sconosciuto', errore);
+    }
+    return [];
+  }
+}
+
+getActors([1,2,3,4])
+
+
+
+function createActor(dati: Omit<Actor, "id">): Actor{
+  return{
+    id:generaID(),
+    ...dati
+  }
+}
+
+function updateActor(actor: Actor, updates: Partial<Omit<Actor, "id" | "name">>): Actor{
+  return{
+    ...actor,
+    ...updates
+  }
+}
+
+
